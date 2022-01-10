58689266ba7fc53b0e4db65063b39260b1596ab5
Voltis Jakupi <voltisjakupi@gmail.com>
2022-01-10 20:38:18 +0100

    yarn removed.

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,16 @@
 const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize("todoexpress", 'root', 'root', {host: '127.0.0.1', dialect: "mysql"})
+//postgres://wjhlwvcbtytkmy:233a481ed43d6359dbb0779b65b22b34486b7cadbdba6ae8c96f11e2b7211137@ec2-52-211-158-144.eu-west-1.compute.amazonaws.com:5432/dfjnm25kkaakb3
+const sequelize = new Sequelize("dfjnm25kkaakb3", 'wjhlwvcbtytkmy', '233a481ed43d6359dbb0779b65b22b34486b7cadbdba6ae8c96f11e2b7211137', {host: 'ec2-52-211-158-144.eu-west-1.compute.amazonaws.com', dialect: "postgres", dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+},
+})
+
+sequelize.sync()
+
 
 module.exports = sequelize
 
