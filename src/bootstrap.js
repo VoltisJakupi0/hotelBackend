c711be91cf917f42fc9f30cfeed5ad1d37dd860c
Voltis Jakupi <voltisjakupi@gmail.com>
2022-01-11 09:39:28 +0100

    remove unused model.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,6 @@
 module.exports  = async () => {
     const User = require("./models/User")
     const Client = require("./models/Client")
-    const Invoice = require("./models/Invoice")
     const Role = require("./models/Role")
     const Room = require("./models/Room")
     const Status = require("./models/Status")
@@ -9,7 +8,6 @@ module.exports  = async () => {
     const Category = require("./models/Category")
 
     User.belongsTo(Role ,{as: "Role", foreignKey: 'role_id'})
-    Invoice.belongsTo(Client ,{as: "Client", foreignKey: 'client_id'})
     Room.belongsTo(Category,{as: "Category", foreignKey: 'category_id'})
     Room.belongsTo(Status,{as: "Status", foreignKey: 'status_id'})
     BookedRoom.belongsTo(Room,{as: "Room", foreignKey: 'room_id'})
